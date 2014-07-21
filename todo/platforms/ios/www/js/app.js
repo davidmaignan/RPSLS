7f57b306565988efd292e941f903994f0b354dfb
David Maignan <davidmaignan@gmail.com>
2014-07-21 11:29:39 -0400

    build ios

--- todo/platforms/ios/www/js/app.js
+++ todo/platforms/ios/www/js/app.js
@@ -5,6 +5,41 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic'])
 
+    .controller('TodoCtrl', function($scope, $ionicModal) {
+        $scope.tasks = [
+            { title: 'Collect coins' },
+            { title: 'Eat mushrooms' },
+            { title: 'Get high enough to grab the flag' },
+            { title: 'Find the Princess' }
+        ];
+
+        // Create and load the Modal
+        $ionicModal.fromTemplateUrl('new-task.html', function(modal) {
+            $scope.taskModal = modal;
+        }, {
+            scope: $scope,
+            animation: 'slide-in-up'
+        });
+
+        $scope.newTask = function() {
+            $scope.taskModal.show();
+        };
+
+        // Close the new task modal
+        $scope.closeNewTask = function() {
+            $scope.taskModal.hide();
+        };
+
+        // Called when the form is submitted
+        $scope.createTask = function(task) {
+            $scope.tasks.push({
+                title: task.title
+            });
+            $scope.taskModal.hide();
+            task.title = "";
+        };
+    })
+
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard

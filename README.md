a88e11e6a68f20d15f732a9c8cb8fd5984e985b6
David Maignan <davidmaignan@gmail.com>
2014-07-21 10:57:00 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 RPCLS
 =====
+
+Install Cordova, make sure you have [Node.js](http://nodejs.org/) installed, then run
+```CLI
+$ sudo npm install -g cordova
+```
+Install Ionic
+```CLI
+$ sudo npm install -g ionic
+```
+
+Usefull commands
+
+Configure Platforms
+```CLI
+$ ionic platform ios
+$ ionic platform android
+```
+
+Test it out
+```CLI
+$ ionic build ios
+$ ionic emulate ios
+```
---
+++ todo/www/js/app.js
@@ -0,0 +1,54 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic'])
+
+    .controller('TodoCtrl', function($scope, $ionicModal) {
+        $scope.tasks = [
+            { title: 'Collect coins' },
+            { title: 'Eat mushrooms' },
+            { title: 'Get high enough to grab the flag' },
+            { title: 'Find the Princess' }
+        ];
+
+        // Create and load the Modal
+        $ionicModal.fromTemplateUrl('new-task.html', function(modal) {
+            $scope.taskModal = modal;
+        }, {
+            scope: $scope,
+            animation: 'slide-in-up'
+        });
+
+        $scope.newTask = function() {
+            $scope.taskModal.show();
+        };
+
+        // Close the new task modal
+        $scope.closeNewTask = function() {
+            $scope.taskModal.hide();
+        };
+
+        // Called when the form is submitted
+        $scope.createTask = function(task) {
+            $scope.tasks.push({
+                title: task.title
+            });
+            $scope.taskModal.hide();
+            task.title = "";
+        };
+    })
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})

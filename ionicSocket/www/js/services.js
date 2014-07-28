4274bd20dc40fa2b4975ea8b40b8e95de9898f91
David Maignan <davidmaignan@gmail.com>
2014-07-28 09:20:28 -0400

    initial commit

---
+++ ionicSocket/www/js/services.js
@@ -0,0 +1,27 @@
+var baseUrl = 'http://localhost:8080/';
+
+angular.module('services', [])
+
+    .factory('socket', function socket($rootScope) {
+        var socket = io.connect(baseUrl);
+        return {
+            on: function (eventName, callback) {
+                socket.on(eventName, function () {
+                    var args = arguments;
+                    $rootScope.$apply(function () {
+                        callback.apply(socket, args);
+                    });
+                });
+            },
+            emit: function (eventName, data, callback) {
+                socket.emit(eventName, data, function () {
+                    var args = arguments;
+                    $rootScope.$apply(function () {
+                        if (callback) {
+                            callback.apply(socket, args);
+                        }
+                    });
+                })
+            }
+        };
+    })

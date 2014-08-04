5e2b8c09a1f7bf1aee30d0583472b2cbef16fcae
David Maignan <davidmaignan@gmail.com>
2014-08-04 13:24:58 -0400

    update api get/players

--- express/app/api.js
+++ express/app/api.js
@@ -11,7 +11,7 @@ module.exports = function(app, mongoose) {
         res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
         // intercept OPTIONS method
-        if ('OPTIONS' == req.method) {
+        if ('GET' == req.method || 'POST' == req.method) {
             res.send(200);
         }
         else {
@@ -32,57 +32,45 @@ module.exports = function(app, mongoose) {
             res.send(200);
         }
         else {
-            next();
+            res.send(500);
         }
-
-        res.json({ message: 'Welcome to RPSLS api!' });
     });
 
     app.get('/api/players', function(req, res) {
-
-        var playerList = [
-            { "id" : "53dbcf635df946aa30d29ae7", "facebook" : { "email" : "davidmaignan@gmail.com", "name" : "David Maignan", "token" : "CAAB6HUWTsJoBAKX0rIbBtjuz6jyZCy9NUbNmeM7G2PFmuEjHe3r6D89ugLYqyUSIoZC64RfyzcVZBweofMSP7zRZAU8trMTyM4A3fAL0MLf7La4TqdPVyK4a6FYZB2WQutzqS71ZBax8QukYjgh0z2omD4BrvZArffCLXMwAiRfaJXvKDZB1mLmT7WOlCtIpZAR1wsyCmlDXVWsAvGzso6R4B", "id" : "1160449371" }, "__v" : 0 },
-            { "id" : "53dbcf635df946aa30d29ae7", "facebook" : { "email" : "davidmaignan@gmail.com", "name" : "David Maignan", "token" : "CAAB6HUWTsJoBAKX0rIbBtjuz6jyZCy9NUbNmeM7G2PFmuEjHe3r6D89ugLYqyUSIoZC64RfyzcVZBweofMSP7zRZAU8trMTyM4A3fAL0MLf7La4TqdPVyK4a6FYZB2WQutzqS71ZBax8QukYjgh0z2omD4BrvZArffCLXMwAiRfaJXvKDZB1mLmT7WOlCtIpZAR1wsyCmlDXVWsAvGzso6R4B", "id" : "1160449371" }, "__v" : 0 },
-            { "id" : "53dbcf635df946aa30d29ae7", "facebook" : { "email" : "davidmaignan@gmail.com", "name" : "David Maignan", "token" : "CAAB6HUWTsJoBAKX0rIbBtjuz6jyZCy9NUbNmeM7G2PFmuEjHe3r6D89ugLYqyUSIoZC64RfyzcVZBweofMSP7zRZAU8trMTyM4A3fAL0MLf7La4TqdPVyK4a6FYZB2WQutzqS71ZBax8QukYjgh0z2omD4BrvZArffCLXMwAiRfaJXvKDZB1mLmT7WOlCtIpZAR1wsyCmlDXVWsAvGzso6R4B", "id" : "1160449371" }, "__v" : 0 },
-            { "id" : "53dbcf635df946aa30d29ae7", "facebook" : { "email" : "davidmaignan@gmail.com", "name" : "David Maignan", "token" : "CAAB6HUWTsJoBAKX0rIbBtjuz6jyZCy9NUbNmeM7G2PFmuEjHe3r6D89ugLYqyUSIoZC64RfyzcVZBweofMSP7zRZAU8trMTyM4A3fAL0MLf7La4TqdPVyK4a6FYZB2WQutzqS71ZBax8QukYjgh0z2omD4BrvZArffCLXMwAiRfaJXvKDZB1mLmT7WOlCtIpZAR1wsyCmlDXVWsAvGzso6R4B", "id" : "1160449371" }, "__v" : 0 }
-        ];
 
         res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
         res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
-        // intercept OPTIONS method
-        if ('GET' == req.method) {
-            res.json(playerList);
-        }
-        else {
-            res.send(500)
-        }
+        var playerCollection = User.find(function(err, collection) {
+            if (err) {
+                res.send(500)
+            }
+            console.log(collection);
+            res.json(collection);
+        });
+
     });
 
     app.post('/api/players', function(req, res) {
-
-        console.log(req.body);
-
         var newUser = new User();
-//
+
         // set all of the facebook information in our user model
         newUser.facebook.id        = req.query.id;
         newUser.facebook.token     = req.query.token;
-        newUser.facebook.firstName = req.query.firstName + ' ' + req.query.familyName;
-        newUser.facebook.lastName  =
+        newUser.facebook.name      = req.query.firstName + ' ' + req.query.lastName;
         newUser.facebook.email     = req.query.email;
         newUser.facebook.online    = true;
         newUser.facebook.createdAt = new Date().getTime();
         newUser.facebook.loggedIn  = new Date().getTime();
-        newUser.win                = 0;
-        newUser.lose               = 0;
+        newUser.win                = req.query.win;
+        newUser.lose               = req.query.lose;
 
         var saveUser = newUser.save(function(err) {
-            if (err)
+            if (err) {
                 return false
+            }
 
-            // if successful, return the new user
             return true
         });
 
@@ -95,9 +83,7 @@ module.exports = function(app, mongoose) {
             res.send(200);
         }
         else {
-            res.json({ message: 'Welcome to RPSLS api!' });
+            res.send(500);
         }
     });
-
-
 }
